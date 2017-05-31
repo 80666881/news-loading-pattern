4127b550dafc41a2f46fc3c5252f1374732ac05c
铁腕阿童木 <87192329@163.com>
2017-05-31 10:03:58 +0800

    task15

---
+++ .router.js
@@ -0,0 +1,16 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/getNews',function(req,res){
+	var index = req.query.index;
+
+	var arr = [];
+	arr.length = 6;
+	for (var i = 0; i<arr.length; i++) {
+		var index = parseInt(index);
+		arr[i] = '内容'+ (index + i + 1);
+	}
+	res.send(arr);
+	// res.send('aaa');
+})}
+ module.exports.setRouter = setRouter
